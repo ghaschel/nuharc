d9a87a8ab7c21ad139a669a273007b27435bc16a
ghaschel <ghaschel@gmail.com>
2019-05-08 22:06:32 -0300

    Moving things around and organizing for extract

---
+++ defaults.js
@@ -0,0 +1,8 @@
+const defaults = {
+  add: {
+   headerEncryption: true, // archive header encryption
+   recursive: true, //should include subfolders
+   clearFileArchiceAttr: true, // clear file archive attr
+   multimediaCompression: true  // enable multimedia detection and compression
+  }
+};
